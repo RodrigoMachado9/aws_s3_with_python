590ddcb7f7ac3988de6e5f6721a3ba10b7be94a6
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-04-11 16:59:47 -0300

    list buckets

--- src/s3_operations.py
+++ src/s3_operations.py
@@ -1,5 +1,5 @@
 import boto3
-
+import json
 BUCKET_NAME = 'rmachado-development'
 
 
@@ -12,11 +12,43 @@ def create_bucket(bucket_name):
     return s3_client().create_bucket(
         Bucket=bucket_name,
         CreateBucketConfiguration={
-            'LocationConstraint': 'us-west-2'
+            "LocationConstraint": "us-west-2"
         }
     )
 
+def create_bucket_policy():
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AddPerm",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": ["s3:*"],
+                "Resource": ["arn:aws:s3:::rmachado-development/*"]
+            }
+        ]
+    }
+    policy_string = json.dumps(bucket_policy)
+    return s3_client().put_bucket_policy(
+        Bucket=BUCKET_NAME,
+        Policy=policy_string
+    )
+
+
+def list_buckets():
+    return s3_client().list_buckets()
+
+
+def get_bucket_policy():
+    return s3_client().get_bucket_policy(
+        Bucket=BUCKET_NAME
+    )
 
 
 if __name__ == '__main__':
-    create_bucket(BUCKET_NAME)
+    # create_bucket(BUCKET_NAME)
+    # print(create_bucket_policy())
+    print(list_buckets())
+    # print(get_bucket_policy())
+
