0b6cc6f7b2bd80ee66749348a5c7d21b49246452
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-04-11 22:03:23 -0300

    added method put policys bucket

--- src/s3_operations.py
+++ src/s3_operations.py
@@ -25,7 +25,7 @@ def create_bucket_policy():
                 "Effect": "Allow",
                 "Principal": "*",
                 "Action": ["s3:*"],
-                "Resource": ["arn:aws:s3:::rmachado-development/*"]
+                "Resource": ["arn:aws:s3:::rmachado-development"]
             }
         ]
     }
@@ -46,9 +46,44 @@ def get_bucket_policy():
     )
 
 
+def get_bucket_encryption():
+    # s3_client().get_paginator()
+    return s3_client().get_bucket_encryption(Bucket=BUCKET_NAME)
+
+
+def get_bucket_cors():
+    return s3_client().get_bucket_cors(Bucket=BUCKET_NAME)
+
+
+def update_bucket_policy():
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AddPerm",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": [
+                    "s3:DeleteObject",
+                    "s3:GetObject",
+                    "s3:PutObject"
+                ],
+                "Resource": "arn:aws:s3:::rmachado-development/*"
+            }
+        ]
+    }
+    policy_string = json.dumps(bucket_policy)
+    return s3_client().put_bucket_policy(
+        Bucket=BUCKET_NAME,
+        Policy=policy_string
+    )
+
+
 if __name__ == '__main__':
     # create_bucket(BUCKET_NAME)
     # print(create_bucket_policy())
-    print(list_buckets())
+    # print(list_buckets())
     # print(get_bucket_policy())
+    # print(get_bucket_encryption())
+    print(update_bucket_policy())
 
